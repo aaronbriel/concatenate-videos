0d06acc4c49cdeb9754df57518e9bb4e1ae4f554
Aaron Briel <aaronbriel@gmail.com>
2019-08-03 10:55:47 -0500

    added sort logic

--- concatenate_videos.py
+++ concatenate_videos.py
@@ -1,16 +1,24 @@
 import os
 import sys
 
-def create_video(_path):
-    text_file_path = _path + "/videos.txt"
-    with open(text_file_path, "w") as text_file:
-        for r, d, f in os.walk(_path):
-            for file in f:
-                if '.mp4' in file:
-                    # files.append("file '{}'".format(file))
-                    text_file.write("file '{}'\n".format(file))
 
-    os.system("ffmpeg -f concat -safe 0 -i {} -c copy video.mp4".format(text_file_path))
+def create_video(_path, sort=True):
+    files = []
+    text_file_path = _path + "/videos.txt"
+
+    for r, d, f in os.walk(_path):
+        for file in f:
+            if '.mp4' in file:
+                files.append("file '{}'\n".format(file))
+
+    with open(text_file_path, "w") as text_file:
+        if sort:
+            files.sort()
+        text_file.write(''.join(files))
+
+    os.system("ffmpeg -f concat -safe 0 -i {} -c copy video.mp4"
+              .format(text_file_path))
+
 
 try:
     path = sys.argv[1]
